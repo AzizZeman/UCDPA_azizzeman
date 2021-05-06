773659cc17f92ec2b15c5ed01a2d89e7528f268f
AzizZeman <azizzeman@gmail.com>
2021-05-06 20:12:45 +0100

    "`Sixth commit"

--- main.py
+++ main.py
@@ -42,21 +42,6 @@ print (total_data.isna().any())
 total_data.isna().sum().plot(kind='bar')
 plt.show()
 
-##The 'Games' column is just the 'Year' and 'Season' column combined
-#new_olympics_data = olympics_data.drop(['Games'], axis = 1)
-
-#print (new_olympics_data.head)
-#print(new_olympics_data.columns)
-
-#we'd like an overview of missing values and show it in a bar-graph
-#print (new_olympics_data.isna().any())
-#new_olympics_data.isna().sum().plot(kind='bar')
-#plt.show()
-
-#we are now going to count the missing values: missing_values = olympics_data.isnull().sum()
-#print(new_olympics_data.isnull().sum())
-
-
 
 #We can see we have missing values in Age, Height, Weight and Medal. Need to clean these if we are going to use them
 #First we calculate the mean age
@@ -90,42 +75,81 @@ print (mean_weight)
 total_data["Weight"]=total_data["Weight"].fillna(mean_weight)
 total_data["Weight"]
 
-#
+#We check the values, see if the change is made
 print (total_data.isnull().sum())
-
 print ('We have now cleaned the Age, Weight and Height columns')
-
-
 print (total_data.isnull().sum())
 print(total_data.isna().any())
 total_data.isna().sum().plot(kind='bar')
 plt.show()
 
-#olympics_data_height=total_data['Height'].fillna(olympics_data['Height'].mean(), inplace = False)
-#print(olympics_data_height.isnull().sum())
+#We now want to change the NaN values in the Medals column to Losers
+total_data['Medal'] = total_data['Medal'].fillna('Losers')
+print (total_data.isnull().sum())
 
-
-
-#we can see we have missing values in Age, Height, Weight and Medal. Need to clean these if we are going to use them
-
-print('olympics data medals 1')
-olympics_data['Medal'].fillna(olympics_data["Loser"], inplace = False)
-print(olympics_data_medals.columns)
-print (olympics_data_medals.head())
-print('olympics data medals 2')
-
-
-print (olympics_data.isna().any())
-new_olympics_data.isna().sum().plot(kind='bar')
+#We check the values, see if the change is made
+print (total_data.isna().any())
+total_data.isna().sum().plot(kind='bar')
 plt.show()
+
+print (total_data.head)
+print(total_data.columns)
+print (type(total_data))
+
+#We go on to grouping by Sex
+data_gender=total_data['Sex'].value_counts()
+print (data_gender.head())
+#data_gender.sum().plot(kind='bar')
+#plt.show()
+
+
+#Now we are going to see how many medals the Netherlands won in each Games
+Netherlands_edition_grouped = total_data.loc[total_data.NOC == 'NED'].groupby('Games')
+print (Netherlands_edition_grouped['Medal'].count().head())
+
+Netherlands_edition_grouped['Medal'].count().plot(kind='bar')
+plt.xlabel('Games')
+plt.ylabel('Amount of medals')
+plt.title('Amount of Medals won by the Netherlands per Edition')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+total_data.plot("Year", "Team")
+plt.show()
+
+
+##The 'Games' column is just the 'Year' and 'Season' column combined
+#new_olympics_data = olympics_data.drop(['Games'], axis = 1)
+#new_total_data = total_data.drop(['Notes', 'Games']), axis = 1)
+#print (new_total_data.head)
+#print(new_total_data.columns)
+
+#we'd like an overview of missing values and show it in a bar-graph
+#print (new_total_data.isna().any())
+#new_total_data.isna().sum().plot(kind='bar')
+#plt.show()
+
+#we are now going to count the missing values: missing_values = olympics_data.isnull().sum()
+#print(new_olympics_data.isnull().sum())
+
+
 
 #print (olympics_data_medals.isna().any())
 #print(olympics_data_medals.shape)
 #print(olympics_data_medals.columns)
 
 
-
 #I found a way to mark the 18th row, and by going into the debugger, check how to see the code as a dataframe//
 #there we found that the row
-
-#here is some more text, lets check it is pushed
